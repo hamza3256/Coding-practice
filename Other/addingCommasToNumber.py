fd941b42246bc3bd0c4af2bf6e500d4796ef8ed7
Hamza <ramzoo3256@gmail.com>
2020-05-19 20:48:49 +0100

    adding more

---
+++ Other/addingCommasToNumber.py
@@ -0,0 +1,7 @@
+def addingCommasToNumber(n):
+    s = str(n); c= 0
+    for i in range(len(s)-1,-1,-1):
+        if (len(s) - i -c) % 3 == 0 and i !=0:
+            s = s[:i] + "," + s[i:]
+            c+=1
+    return s
